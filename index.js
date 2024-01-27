87f63b943f14b6ac270039864bf46ddacebe630d
vargamolnarbertalan <vargamolnarb@gmail.com>
2024-01-27 21:55:33 +0100

    v0.8.0

--- index.js
+++ index.js
@@ -13,6 +13,28 @@ app.use(express.static('public'))
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ port: 13136 })
 
+var lastMSG
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log(message.toString())
+        if(message.toString() == 'LOADED'){
+            wss.clients.forEach((client) => {
+                client.send(lastMSG)
+            })
+        }
+        // Broadcast the received message to all connected clients
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                var object = message.toString()
+                    //console.log(object)
+                    lastMSG = object
+                client.send(object)
+            }
+        })
+    })
+})
+
 app.get('/', (req, res) => {
     res.render('index')
 })
@@ -21,19 +43,18 @@ app.get('/follow', (req, res) => {
     res.render('output')
 })
 
+app.get('/hidenext', (req, res) => {
+
+    wss.clients.forEach((client) => {
+        // Send a message to all connected WebSocket clients
+        client.send('HIDENEXT')
+      })
+    
+      res.send('DONE')
+
+})
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
 })
 
-wss.on('connection', (ws) => {
-    ws.on('message', (message) => {
-        // Broadcast the received message to all connected clients
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                var object = message.toString()
-                    //console.log(object)
-                client.send(object)
-            }
-        })
-    })
-})
