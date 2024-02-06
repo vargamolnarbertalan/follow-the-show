f31c5e3900ae14a26a8e913e58001b07786407f7
vargamolnarbertalan <vargamolnarb@gmail.com>
2024-02-06 23:04:03 +0100

    v1.0.2

--- run.bat
+++ run.bat
@@ -1,1 +1,19 @@
+@echo off
+setlocal
+
+REM Check if node_modules directory exists
+if not exist "node_modules" (
+    echo node_modules directory not found. Running npm ci...
+    npm ci
+    REM Run node index.js
+    echo Running node index.js...
+    node --no-warnings index.js
+
+    endlocal
+) else ( REM Run node index.js
+echo Running node index.js...
 node --no-warnings index.js
+
+endlocal )
+
+
